84290562782bc42a98ef8a71302d197f606abb5f
權少 <ljqican@163.com>
2021-01-17 21:55:45 +0800

    重新學習，從基礎學起。

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
 import React, { FC, useState, useCallback } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import { Tabs, Input, Button } from 'antd';
-import { Game } from './Game';
-import { ToDo } from './ToDo';
-// import { useImmer } from 'use-immer';
-import { range } from "rxjs";
-import { map, filter } from "rxjs/operators";
-import { Native } from './Native';
-import { RxJS } from './RxJS';
-
-const { TabPane } = Tabs;
+import { useImmer } from 'use-immer';
 
 // function App() {
 //   return (
@@ -33,29 +24,72 @@ const { TabPane } = Tabs;
 //   );
 // }
 
-const App: FC = () => {
-  // const [checked, setChecked] = useState<boolean>(false);
+class ToDo {
+  id: number = 0;
+  content: string = "";
+
+  constructor() {
+  }
+}
+
+function useToDoService() {
+  const [list, updateList] = useImmer<ToDo[]>([]);
+  const [edit, setEdit] = useState<string>("");
+  const [maxId, setMaxId] = useState<number>(0);
+
+  const add = useCallback((value: string) => {
+    if (value === "") {
+      alert("不能為空。");
+    }
+    updateList(draft => {
+      let toDo = new ToDo();
+      toDo.id = maxId;
+      toDo.content = value;
+      draft.push(toDo);
+      setEdit("");
+      setMaxId(maxId + 1);
+    });
+  }, [updateList, setEdit, maxId, setMaxId]);
+
+  const remove  = useCallback((id: number) => {
+    const idx = list.findIndex(v => v.id === id);
+    if (idx !== -1) {
+      updateList(draft => {
+        draft.splice(idx, 1);
+      });
+    }
+  }, [updateList, list]);
+
+  return {
+    edit,
+    setEdit,
+    list,
+    add,
+    remove
+  };
+}
+
+function App() {
+  const toDoService = useToDoService();
 
   return (
-    <Tabs defaultActiveKey="5">
-      <TabPane tab="游戲" key="1">
-        <Game />
-      </TabPane>
-      <TabPane tab="待辦" key="2">
-        <ToDo />
-      </TabPane>
-      <TabPane tab="控件" key="3">
-        <Button onClick={() => alert('click')}>Button</Button>
-        <Input />
-      </TabPane>
-      <TabPane tab="原生" key="4">
-        <Native />
-      </TabPane>
-      <TabPane tab="RxJS" key="5">
-        <RxJS />
-      </TabPane>
-    </Tabs>
+    <div>
+      <input type="text"
+        value={toDoService.edit}
+        onChange={e => toDoService.setEdit(e.target.value)} />
+      <button onClick={e => toDoService.add(toDoService.edit)}>+</button>
+      <ul>
+        {toDoService.list.map((v, i) => {
+          return (
+            <li>
+              {v.content}
+              <button onClick={e => toDoService.remove(v.id)}>-</button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
-};
+}
 
 export default App;
